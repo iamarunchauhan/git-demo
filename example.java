708b4a679da277f4db04ec302f8c6b14ba9e54b8
iamarunchauhan <arunchauhan2319@gmail.com>
2023-05-14 18:02:41 +0530

    Added loop

--- example.java
+++ example.java
@@ -1,4 +1,11 @@
 public static void main(String args[]) {
+
+    int val = 10;
+
+    for(int i=0; i<val; i++){
+       System.out.println(i); 
+    }
+
     System.out.println("This is demonstration of Git Working - Using Dev branch");
     System.out.println("Done!");
 }
